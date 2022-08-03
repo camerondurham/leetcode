bdaa415cc9e47c020cc11dead2532a0f857a4fd4
cam <u64.cam@gmail.com>
2022-08-02 23:58:31 -0700

    cleanup k closest points

--- 2022/k-closest-points/Solution.cpp
+++ 2022/k-closest-points/Solution.cpp
@@ -14,18 +14,12 @@ int main() {
     auto euclidian = [](const std::vector<int>& elem) {
         return (elem[0] * elem[0]) + (elem[1] * elem[1]);
     };
-    auto comparator = [=](const std::vector<int>& a, const std::vector<int>& b) {
+    auto comparator = [euclidian](const std::vector<int>& a, const std::vector<int>& b) {
         return euclidian(a) > euclidian(b);
     };
     std::make_heap(begin(numbers), end(numbers), comparator);
     int k = 0;
     int n = 2;
-//    std::vector<std::vector<int>> soln;
-//    for(const auto elem : numbers) {
-//        soln.push_back(elem);
-//    }
-//    std::make_heap(begin(soln), end(soln), comparator);
-//    while(soln.size() > n) { soln.pop_back(); }
     Solution solution;
     auto soln = solution.kClosest(numbers, 2);
 
