26e45ba9d34e8954a212f1df871b2246235939ee
Cameron Durham <camdurha@amazon.com>
2022-08-16 05:49:55 -0700

    Add faster LL solution

--- 2022/linked-list-cycle-ii/Solution.cpp
+++ 2022/linked-list-cycle-ii/Solution.cpp
@@ -9,27 +9,47 @@ using std::unordered_set;
  * };
  */
 struct ListNode {
-   int val;
-   ListNode *next;
-   ListNode() : val(0), next(nullptr) {}
-   ListNode(int x) : val(x), next(nullptr) {}
-   ListNode(int x, ListNode *next) : val(x), next(next) {}
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 class Solution {
-public:
-  
+  public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> seen;
-        ListNode* tmp = head;
+      if (head == nullptr || head -> next == nullptr) return nullptr;
+      ListNode* fast = head, *slow = head;
 
-        while(tmp != nullptr) {
-            if (seen.find(tmp) == seen.end()){
-                seen.insert(tmp);
-                tmp = tmp->next;
-            } else {
-                return tmp;
-            }
+      while (fast != nullptr && fast->next != nullptr) {
+        fast = fast -> next -> next;
+        slow = slow -> next;
+        // once loop is found, need to figure out where it starts so begin from the head of the list
+        if (fast == slow) {
+          ListNode* entry = head;
+          while(entry != slow) {
+            entry = entry->next;
+            slow = slow->next;
+          }
+          return slow;
+        } 
+
+      }
+      return nullptr;
+    }
+
+    ListNode *detectCycleMemoryHeavy(ListNode *head) {
+      unordered_set<ListNode*> seen;
+      ListNode* tmp = head;
+
+      while(tmp != nullptr) {
+        if (seen.find(tmp) == seen.end()){
+          seen.insert(tmp);
+          tmp = tmp->next;
+        } else {
+          return tmp;
         }
-        return nullptr;
+      }
+      return nullptr;
     }
 };
