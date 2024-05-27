dc79d97a5f663f1b5f28accbb267717f54816f62
Cameron Durham <u64.cam@gmail.com>
2024-05-26 21:36:33 -0700

    Add more optimal solution

--- problems/0347_top_k_frequent_elements.py
+++ problems/0347_top_k_frequent_elements.py
@@ -16,3 +16,26 @@ class BasicSolution:
         for (k,v) in top:
             answ.append(v) # add the answer to the array
         return answ
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        # used for "bucket sort"
+        # length can be at most len(nums) since that's the most frequent it can occur in the array
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        # build the array where the index = frequency occurred, value(s) are numbers that occur that frequently
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # complexity of this is O(n) to get frequency count, O(n) to go through and find the top K most frequent
+        # so total complexity is O(n)
+        # space complexity is also O(n)
