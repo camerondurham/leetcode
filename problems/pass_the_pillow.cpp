628eb9de511566fd15a88f3054b78aefff72098f
Cam <17013462+camerondurham@users.noreply.github.com>
2024-07-05 23:56:57 -0700

    Create pass_the_pillow.cpp

---
+++ problems/pass_the_pillow.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int counter = 0;
+        bool increase = true;
+        int current = 1;
+        while (counter < time) {
+            if (increase && current+1 <= n) {
+                current++;
+            } else if (increase && current+1 > n) {
+                increase = false;
+                current--;
+            } else if (!increase && current-1 >= 1) {
+                current--;
+            } else if (!increase && current-1 < 1) {
+                increase = true;
+                current++;
+            }
+            counter++;
+        }
+        return current;
+    }
+    // brute force just keep track of the direction and run the simulation until you get to 1/n
+};
